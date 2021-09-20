d46ae1dcacdd5c5cf290af7e7ac2a78da36a4999
XiminHu <xhu66@uw.edu>
2021-09-20 13:14:22 -0700

    mp_class is finished, multiple file multiprocessing tested on 3 files

--- dev/mp_class.py
+++ dev/mp_class.py
@@ -3,18 +3,30 @@ import sys
 # import mss
 sys.path.append('../')
 from mss import mssmain as msm
+from mss import align
 import multiprocessing
 from timeit import default_timer as timer
+import pandas as pd
 
 if __name__ == '__main__':
     start = timer()
     path = '../example_data/'
-    all_scans, file_list = msm.batch_scans(path, remove_noise=True, thres_noise=5000)
+    all_scans, file_names = msm.batch_scans(path, remove_noise=True, thres_noise=5000)
+    file_list = [path + str(i) + '.csv' for i in file_names]
+    files = list(zip(all_scans, file_list))
     jobs = []
-    for scans in all_scans:
-        p = multiprocessing.Process(target=msm.peak_list, args=(scans,))
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    for scans in files:
+        p = multiprocessing.Process(target=msm.mp_peak_list, args=(scans[0],scans[1],return_dict))
         jobs.append(p)
         p.start()
 
+    for proc in jobs:
+        proc.join()
+
+    df_list = return_dict.values()
+    alignment = align.mss_align(df_list, '../example_data/test.csv', file_names, RT_error = 0.5, mz_error = 0.02)
     end = timer()
     print(f'elapsed time: {end - start}')
+
--- mss/mssmain.py
+++ mss/mssmain.py
@@ -525,3 +525,64 @@ def formula_prediction(mzml_scan, input_mz, error=10, mfRange='C0-100H0-200N0-20
     prediction_table.insert(0,'Input Mass', precursor_mz)
 
     return prediction_table
+
+
+def mp_peak_list(mzml_scans, file_name, return_dict, err_ppm=10, enable_score=False, mz_c_thres=5,
+              peak_base=0.005, peakutils_thres=0.02, min_d=1, rt_window=1.5,
+              peak_area_thres=1e5, min_scan=5, max_scan=50,
+              max_peak=5):
+    '''
+    Generate a dataframe by looping throughout the
+    whole mz space from a given mzml file
+    ref to peak_picking function
+    all the parameters included in peak_pick
+    mz_c_thres: defines how much mz need to be within a cluster for
+    a valid precursor in peak list detection
+    '''
+
+    # Get m/z range -- updated 0416
+    print('Generating mz list...')
+
+    mzlist = mz_gen(mzml_scans, err_ppm, mz_c_thres)
+    print('Finding peaks...')
+
+    result_dict = {}
+    rt = [i.scan_time[0] for i in mzml_scans]
+
+### multiprocessing
+    for mz in mzlist:
+        try:
+            peak_dict = peak_pick(mzml_scans, mz, err_ppm, enable_score,
+                                  peak_thres=peak_base,
+                                  peakutils_thres=peakutils_thres,
+                                  min_d=min_d, rt_window=rt_window,
+                                  peak_area_thres=peak_area_thres,
+                                  min_scan=min_scan, max_scan=max_scan,
+                                  max_peak=max_peak, rt = rt)
+        except Exception:  # Catch exception?
+            peak_dict = {}
+
+        if len(peak_dict) != 0:
+            if len(result_dict) == 0:
+                for index in peak_dict:
+                    result_dict.update({'m/z': [mz],
+                                        'rt': [rt[index]],
+                                        'sn': [peak_dict[index][3]],
+                                        'score': [peak_dict[index][4]],
+                                        'peak area': [peak_dict[index][2]]})
+            else:
+                for index in peak_dict:
+                    result_dict['m/z'].append(mz)
+                    result_dict['rt'].append(rt[index])
+                    result_dict['sn'].append(peak_dict[index][3])
+                    result_dict['score'].append(peak_dict[index][4])
+                    result_dict['peak area'].append(peak_dict[index][2])
+    # print(result_dict)
+    print('Peak processing finished!')
+    d_result = pd.DataFrame(result_dict)
+    d_result['rt'] = round(d_result['rt'], 2)
+    d_result['m/z'] = round(d_result['m/z'], 4)
+    print('Dataframe created!')
+    # d_result.to_csv(file_name)
+
+    return_dict[file_name] = d_result
